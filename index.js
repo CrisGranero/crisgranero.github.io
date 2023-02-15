4e3a66f54f4f1a54056286692bcb456df722c715
CrisGranero <125411686+CrisGranero@users.noreply.github.com>
2023-02-14 21:00:23 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("Trabajo en Laboratorio Industrial (2004-2011)");
+    document.getElementById("experiencia1").innerHTML = "Analista de laboratorio medioambiental y responsable de área.";  
+    
+}
+ 
+document.getElementById("boton1").onclick = function () {
+    console.log("Trabajo en escuela secundaria (2012-2021)");
+    document.getElementById("experiencia2").innerHTML = "Docente y esponsable del área de química de 4to y 6to año de secundaria."; 
+    
+   
+}
+
+
